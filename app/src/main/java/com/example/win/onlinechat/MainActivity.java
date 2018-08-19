7b28ecea0536c5e21034d00761cf0dcdf74149a6
Oleg <nester.ole9@gmail.com>
2018-08-19 17:33:22 +0300

    Chat

---
+++ app/src/main/java/com/example/win/onlinechat/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.win.onlinechat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static  int MAX_MESSAGE_LENGTH = 150;
+    FirebaseDatabase database = FirebaseDatabase.getInstance(); //получає силку на бд.
+    DatabaseReference myRef = database.getReference("messages");//получає доступ на блок де зберігаються дані
+
+    EditText mEditTextMessage;
+    Button mSendButton;
+    RecyclerView mMessagesRecycler;
+
+    ArrayList<String> messages = new ArrayList<>();
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSendButton = findViewById(R.id.send_message_b);
+        mEditTextMessage = findViewById(R.id.message_input);
+        mMessagesRecycler = findViewById(R.id.message_recycler);
+
+        mMessagesRecycler.setLayoutManager(new LinearLayoutManager(this));
+
+        final DataAdapter dataAdapter = new DataAdapter(this, messages);
+
+        mMessagesRecycler.setAdapter(dataAdapter);
+
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String msg = mEditTextMessage.getText().toString();
+
+                if (msg.equals("")){
+                    Toast.makeText(getApplicationContext(), "Введите сообщение!", Toast.LENGTH_SHORT) .show();
+                    return;
+                }
+
+                if (msg.length() == MAX_MESSAGE_LENGTH){
+                    Toast.makeText(getApplicationContext(), "Слишком длинное сообщение!", Toast.LENGTH_SHORT) .show();
+                    return;
+                }
+
+                myRef.push().setValue(msg);
+                mEditTextMessage.setText("");
+
+            }
+        });
+
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                String msg = dataSnapshot.getValue(String.class);
+                messages.add(msg);
+                dataAdapter.notifyDataSetChanged();
+                mMessagesRecycler.smoothScrollToPosition(messages.size());
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/win/onlinechat/ViewHolder.java
@@ -0,0 +1,15 @@
+package com.example.win.onlinechat;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+public class ViewHolder extends RecyclerView.ViewHolder {
+
+    TextView message;
+
+    public ViewHolder(View itemView){
+        super(itemView);
+        message = itemView.findViewById(R.id.message_item);
+    }
+}
